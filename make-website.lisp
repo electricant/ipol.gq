d3b81f8185ff0c724d4e812843c2a7e5d3f03a2b
Electric Ant <electricant@anche.no>
2017-01-26 18:06:23 +0100

    The compiler is taking shape

--- make-website.lisp
+++ make-website.lisp
@@ -39,10 +39,45 @@
 ;;; Drectory containing the articles to be compiled
 (defparameter *articles-dir* "content/articles/")
 
+;;; Character encoding to be used (has to be an external-format object)
+(defparameter *char-enc* (flexi-streams:make-external-format :utf-8))
+
 ;;;
 ;;; Various functions and code
 ;;;
 
+;;; Convert a markdown file to html and return the content as a string
+(defun render-file (filename)
+  (with-open-file (in filename)
+  	(let ((data (make-string (file-length in))))
+  	  (read-sequence data in)
+  	  (cl-markdown:render-to-stream (cl-markdown:markdown data :stream nil)
+  	  								:html nil))))
+
+;;; Return a new stream with the encoding set by *char-enc*
+(defun set-encoding (strm)
+  (flexi-streams:make-flexi-stream strm	:external-format *char-enc*))
+
+;;; Strip the provided path for the specified amount of directories
+;;; Parameters;
+;;;  path - path to strip
+;;;  num  - number of directores to remove
+(defun strip-path (path num)
+  (cond
+  	((eql num 0) path)
+  	(t (strip-path (subseq path (1+ (position #\/ path))) (1- num)))))
+
+;;; Returns the list of markdown files to be compiled
+(defun get-sources ()
+  (append (directory (concatenate 'string *pages-dir* "*.md"))
+          (directory (concatenate 'string *articles-dir* "*.md"))))
+
+;;; Build an association list containing the path of each file and its
+;;; destination file where the compiled result will be stored
+
+;;; for each element e of get-sources
+;;;	   append to a new list ((namestring e) ("website/[articles/]"
+;;;	   									     (pathname-name e) ".html")
 
 ;;; Make the website using the functions above
 (defun make-website ()

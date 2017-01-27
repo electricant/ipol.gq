63168b36f4b07fc6259dacac585c9c7e37076ec2
Electric Ant <electricant@anche.no>
2017-01-27 12:39:13 +0100

    Build an a-list of sources and destinations

--- make-website.lisp
+++ make-website.lisp
@@ -33,11 +33,11 @@
 ;;; Directory where the result is stored
 (defparameter *target-dir* "websites/")
 
-;;; Directory containing the pages to be compiled
-(defparameter *pages-dir* "content/pages/")
+;;; Directory containing the pages to be compiled and their extension
+(defparameter *pages-dir* "content/pages/*.md")
 
-;;; Drectory containing the articles to be compiled
-(defparameter *articles-dir* "content/articles/")
+;;; Drectory containing the articles to be compiled and their extension
+(defparameter *articles-dir* "content/articles/*.md")
 
 ;;; Character encoding to be used (has to be an external-format object)
 (defparameter *char-enc* (flexi-streams:make-external-format :utf-8))
@@ -58,26 +58,27 @@
 (defun set-encoding (strm)
   (flexi-streams:make-flexi-stream strm	:external-format *char-enc*))
 
-;;; Strip the provided path for the specified amount of directories
-;;; Parameters;
-;;;  path - path to strip
-;;;  num  - number of directores to remove
-(defun strip-path (path num)
-  (cond
-  	((eql num 0) path)
-  	(t (strip-path (subseq path (1+ (position #\/ path))) (1- num)))))
-
-;;; Returns the list of markdown files to be compiled
+;;; Scan the sources directories and build a list of paths as strings
 (defun get-sources ()
-  (append (directory (concatenate 'string *pages-dir* "*.md"))
-          (directory (concatenate 'string *articles-dir* "*.md"))))
+  (append (mapcar #'namestring (directory *pages-dir*))
+  		  (mapcar #'namestring (directory *articles-dir*))))
+
+;;; Scan the sources directories and bouild a list of paths for compiled html
+;;; targets
+(defun get-destinations ()
+  (append (mapcar 
+  			#'(lambda (p) (concatenate 'string "website/"
+  									   (pathname-name p) ".html"))
+  			(directory *pages-dir*))
+  		  (mapcar
+  		  	#'(lambda (p) (concatenate 'string "website/articles/"
+  		  							   (pathname-name p) ".html"))
+  		  	(directory *articles-dir*))))
 
 ;;; Build an association list containing the path of each file and its
 ;;; destination file where the compiled result will be stored
-
-;;; for each element e of get-sources
-;;;	   append to a new list ((namestring e) ("website/[articles/]"
-;;;	   									     (pathname-name e) ".html")
+(defun build-file-alist ()
+  (pairlis (get-sources) (get-destinations)))
 
 ;;; Make the website using the functions above
 (defun make-website ()
